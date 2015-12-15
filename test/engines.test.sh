1cc60ea8a0762699e6fd95a566f506f68003c993
Oliver Gondža <ogondza@gmail.com>
2015-12-15 14:45:44 +0100

    Fix google engine

--- test/engines.test.sh
+++ test/engines.test.sh
@@ -6,6 +6,6 @@ if [ $? != 0 ]; then
     fail "Command should succeed. Exit code $?"
 fi
 
-if [[ "$out" != 'https://www.google.cz/search?q=nee dle' ]]; then
+if [[ "$out" != 'https://www.google.com/search?q=nee dle' ]]; then
     fail "Should start BROWSER with assembled url. Actual: $out"
 fi
