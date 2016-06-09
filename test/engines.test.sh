01bf280b0e0cbcd75e8ed18898ed93db9fdf1617
Oliver Gondža <ogondza@gmail.com>
2016-06-09 21:46:49 +0200

    Add metal-archives.com engine

--- test/engines.test.sh
+++ test/engines.test.sh
@@ -9,3 +9,14 @@ fi
 if [[ "$out" != 'https://www.google.com/search?q=nee dle' ]]; then
     fail "Should start BROWSER with assembled url. Actual: $out"
 fi
+
+
+TST="Use URL function"
+out=$("$SRCH" metal-archives.com -g death)
+if [ $? != 0 ]; then
+    fail "Command should succeed. Exit code $?"
+fi
+
+if [[ "$out" != *'.com/search?searchString=death&type=band_genre' ]]; then
+    fail "Should start BROWSER with assembled url. Actual: $out"
+fi
